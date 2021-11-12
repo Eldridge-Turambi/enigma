64ae0c15f60bba61925769ef36bce6cfc8620ad9
Eldridge Turambi <eturambi95@gmail.com>
2021-11-12 12:25:21 -0700

    Create #random_five_digit_string test and #todays_date test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,4 +3,14 @@ class Enigma
   def initialize
 
   end
+
+  def random_five_digit_string
+    numbers_array = (0..99999).to_a
+    random_number = numbers_array.sample
+    random_number.to_s.rjust(5, '0')
+  end
+
+  def todays_date
+    Time.now.strftime('%d%m%y')
+  end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -4,10 +4,25 @@ require './lib/enigma'
 
 
 RSpec.describe Enigma do
+  before :each do
+    @enigma = Enigma.new
+  end
 
   it 'exists' do
-    enigma1 = Enigma.new
+    expect(@enigma).to be_an_instance_of(Enigma)
+  end
 
-    expect(enigma1).to be_an_instance_of(Enigma)
+  it '#random_five_digit_string' do
+    expect(@enigma.random_five_digit_string).to be_a(String)
+    expect(@enigma.random_five_digit_string.split(//).count).to eq(5)
+  end
+
+  it '#todays_date' do
+    expect(@enigma.todays_date).to be_a(String)
+    expect(@enigma.todays_date).to eq(Time.now.strftime('%d%m%y'))
+  end
+
+  xit '#encrypt' do
+    expect(@enigma.encrypt('hello world', '02715', '040895')).to be_a(Hash)
   end
 end
