a924226ece6c75ec88761596bfa1c2b6973d1f29
Eldridge Turambi <eturambi95@gmail.com>
2021-11-15 15:16:08 -0700

    Fixed decrypt method. Need special charachter acountability

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -4,11 +4,11 @@ enigma_1 = Enigma.new
 
 input_file = ARGV[0]
 output_file = ARGV[1]
-key = ARGV[2].to_s
-date = ARGV[3].to_s
+key = ARGV[2]
+date = ARGV[3]
 
 
-encrypted_txt = File.new(input_file).read
+encrypted_txt = File.read(input_file).delete!("\n")
 
 File.open(output_file, "w") do |file|
   result_hash = enigma_1.decrypt(encrypted_txt, key, date)
